7210b9b7e3ff7c88c298d62b629d781a03695bfb
Chinmay Rajesh Khiste <chinmaykhiste2006@gmail.com>
2025-12-04 23:26:20 +0530

    commit

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ app.use(cors());
 app.use(express.json());
 
 const dbConfig = {
-  host: process.env.host,
-  user: process.env.user,
-  password: process.env.password, 
-  database: process.env.database,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 };
 
 const PORT = 3001;
